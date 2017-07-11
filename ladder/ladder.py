b6b728ecab4094d8041b8d1b0d7b359eec17af48
Thien-Bach Huynh <tbhuynh10@gmail.com>
2017-07-11 14:37:01 -0400

    finished word ladder
    done

--- ladder/ladder.py
+++ ladder/ladder.py
@@ -11,6 +11,7 @@
 """
 import argparse
 import math
+import string
 from collections import deque
 
 parser = argparse.ArgumentParser()
@@ -28,11 +29,13 @@ class Stack:
     Halves the array if stack is too large.
     """
 
-    def __init__(self):
+    def __init__(self, arr=None):
         self.__array = []
+        if arr:
+            self.__array = arr
         self.__len = len(self.__array)
-        self.__last_index = -1
-        self.__size = 0
+        self.__last_index = self.__len - 1
+        self.__size = self.__len
 
     def resize(self, capacity):
         copy = [None] * capacity
@@ -56,7 +59,7 @@ class Stack:
 
     def pop(self):
         # halve the array if array is too large
-        if self.__last_index < 0:
+        if self.is_empty():
             raise IndexError("Can't pop from an empty stack")
 
         if self.__last_index <= math.floor(self.__len / 4):
@@ -69,8 +72,7 @@ class Stack:
         return val
 
     def __str__(self):
-        for e in self.__array:
-            print("{0}\n".format(e))
+        return ' -> '.join([str(e) for e in self.__array if e != None])
 
     def __len__(self):
         return self.__len
@@ -78,6 +80,12 @@ class Stack:
     def size(self):
         # actual size of internal container
         return self.__size
+
+    def is_empty(self):
+        return self.__len == 0
+
+    def copy(self):
+        return [e for e in self.__array if e != None]
 
 
 class Ladder:
@@ -97,23 +105,65 @@ class Ladder:
     def both_words_in_dict(self, word1, word2):
         return self.word_in_dict(word1) and self.word_in_dict(word2)
 
+    def validate(self, word1, word2):
+        if not self.both_words_in_dict(word1, word2):
+            raise ValueError("Neither words were found in the dictionary!")
+
+        if not self.word_in_dict(word1):
+            raise ValueError("Word1 not in dictionary.")
+
+        if not self.word_in_dict(word2):
+            raise ValueError("Word2 not in dictionary!")
+
+        if len(word1) != len(word2):
+            raise ValueError("Both words must be equal.")
+
+        if word1 == word2:
+            raise ValueError("Words must be different.")
+
     def find_ladder(self, word1, word2):
         """
         BFS implementation. 
         Find all valid neighbors between word1 and word2 by examining every 
         permutation of words from the word at the top of the first stack in the queue. 
-
+        Returns the shortest path if the path is found or -1.
         """
+        self.validate(
+            word1, word2)  # check that these words are actually in dict
         if self.both_words_in_dict(word1, word2):
             # create a queue of word ladder stack
-            stack = [word1]
+            stack = Stack([word1])
             queue = deque([stack])
-            seen = set(stack)
+            seen = set([word1])
             while len(queue) > 0:
                 stack = queue.popleft()
                 # examine neighbors from the current word at the top of the stack
                 curr_word = stack.pop()
-            # if the neighbor not seen and neighbor is word2
+                for i, c in enumerate(curr_word):
+                    for s in string.printable:
+                        neighbor = curr_word[0:i] + s + \
+                            curr_word[i + 1::]
+                        if self.word_in_dict(neighbor) and neighbor not in seen:
+                            # if the neighbor not seen and neighbor is word2
+                            if neighbor == word2:
+                                print("ladder from {0} to {1} is: ".format(
+                                    word2, word1))
+                                # account for the current popped word and first word, hence 2
+                                l = len(stack) + 2
+                                print("{0}, {1}".format(
+                                    word2, curr_word), end="")
+                                while not stack.is_empty():
+                                    print(
+                                        ", {0}".format(stack.pop()), end="")
+                                print("\nlength: ", l)
+                                return l
+                            else:
+                                copy = Stack(stack.copy())
+                                # add this neighbor to the stack's copy
+                                copy.push(neighbor)
+                                seen.add(neighbor)
+                                queue.appendleft(copy)
+            return -1
 
 
 if __name__ == '__main__':
@@ -124,3 +174,4 @@ if __name__ == '__main__':
         if len(args.w1) != len(args.w2):
             raise ValueError("Words must be of the same length")
         ladder = Ladder(filename=args.dict)
+        print(ladder.find_ladder(args.w1, args.w2))
--- ladder/test_ladder.py
+++ ladder/test_ladder.py
@@ -23,16 +23,52 @@ class TestStack(unittest.TestCase):
         self.assertEqual(len(self.stack), 2)
         self.assertEqual(self.stack.pop(), 3)
         self.assertEqual(self.stack.pop(), 1)
-        self.assertRaises(IndexError, self.stack.pop())
+        self.assertRaises(IndexError, self.stack.pop)
+        self.stack.push(4)
+        self.assertEqual(len(self.stack), 1)
+        self.assertEqual(self.stack.pop(), 4)
+
+    def test_is_empty(self):
+        self.assertTrue(self.stack.is_empty())
+        self.stack.push(3)
+        self.stack.push(3)
+        self.stack.pop()
+        self.stack.pop()
+        self.assertTrue(self.stack.is_empty())
+
+    def test_pass_arr_to_const(self):
+        stack = ladder.Stack([1, 2, 3])
+        self.assertEqual(len(stack), 3)
+
+    def test_copy_stack(self):
+        self.stack.push(1)
+        self.stack.push(2)
+        self.stack.push(3)
+        self.assertEqual(self.stack.copy(), [1, 2, 3])
 
 
 class TestLadder(unittest.TestCase):
+    def setUp(self):
+        self.ladder = ladder.Ladder(filename="../res/smalldict1.txt")
+
     def test_words_membership_in_dictionary(self):
         lad = ladder.Ladder(test_dict={"aa", "bb"})
         self.assertTrue(lad.both_words_in_dict("aa", "bb"))
         self.assertTrue(lad.word_in_dict("aa"))
         self.assertFalse(lad.both_words_in_dict("aa", "foo"))
         self.assertFalse(lad.word_in_dict("fdfd"))
+        self.assertRaises(ValueError, lad.validate,
+                          "fdfdasl", "dlafjkljfdsla")
+
+    def test_find_ladder(self):
+        self.assertEqual(self.ladder.find_ladder('code', 'data'), 5)
+        self.assertRaisesRegex(
+            ValueError, "Both words must be equal.", self.ladder.find_ladder, 'ghost', 'boo')
+        self.assertRaisesRegex(ValueError, "Neither words were found in the dictionary!", self.ladder.find_ladder,
+                               '12345', 'eeeef')
+        self.assertEqual(self.ladder.find_ladder('dog', 'cat'), 4)
+        self.assertRaisesRegex(
+            ValueError, "Words must be different.", self.ladder.find_ladder, 'dog', 'dog')
 
 
 if __name__ == '__main__':
