6a8963cac453c94b7ec4caed246b3b9ad8f66341
thien <thien-bach@hingehealth.com>
2017-11-26 19:38:37 -0800

    fixing logic

--- markov/markov.py
+++ markov/markov.py
@@ -1,62 +1,63 @@
 # All of the necessary classes and functions for creating an n-gram, processing text
 # and generating our markov text
 import redis
-
+import random
 
 BEGIN = '_BEGIN_'
 END = '_END_'
 N = 3 # start with a 3 gram. This needs to be > 1
 
-def store_ngram(storage, prefix, suffix, debug=False):
-    # takes a tuple start and the next state,
-    # stores stat and next state as child of that start along with frequency of next state
-    # NOTE: I really don't like mutating the 'storage' state like this. might want to refactor to a class
-    if not(prefix or suffix):
-        raise Exception('Start or next_state are required!')
+class Markov:
+    def __init__(self, storage, debug=False):
+        # TODO: implement redis as non-debug storage
+        self.storage = storage
+        self.debug = debug
 
-    if debug and type(storage) is dict:
-        # if we're in debug mode, storage is a passed dict
-        if prefix not in storage:
-            storage[prefix] = {}
-            storage[prefix]['total_suffixes'] = 0
+    def store_ngram(self, prefix, suffix):
+         """
+         takes a tuple start and the next state,
+         stores stat and next state as child of that start along with frequency of next state
+         NOTE: I really don't like mutating the 'storage' state like this. might want to refactor to a class
+         """
+        if not(prefix or suffix):
+            raise Exception('Start or next_state are required!')
 
-        if suffix not in storage[prefix]:
-            storage[prefix][suffix] = 0
+            # if we're in debug mode, storage is a passed dict
+        if prefix not in self.storage:
+            self.storage[prefix] = {}
+            self.storage[prefix]['total_suffixes'] = 0
 
-        storage[prefix][suffix] += 1
-        storage[prefix]['total_suffixes'] += 1
-        return storage
+        if suffix not in self.storage[prefix]:
+            self.storage[prefix][suffix] = 0
+
+            self.storage[prefix][suffix] += 1
+            self.storage[prefix]['total_suffixes'] += 1
 
 
-def n_gram(run, debug=False):
-    # take a string quote and produce an n-gram
-    assert type(run) is str, 'quote is not a string %s' % run
-    assert run is not '', 'Cannot process empty quote'
-    items = run.split(' ')
-    assert len(items) > N, 'Cannot process strings less than size N'
+    def generate_n_gram(self, run):
+        # take a string quote and produce an n-gram
+        assert type(run) is str, 'quote is not a string %s' % run
+        assert run is not '', 'Cannot process empty quote'
+        items = run.split(' ')
+        assert len(items) > N, 'Cannot process strings less than size N'
 
-    storage = {} if debug else redis.StrictRedis(
-        host='localhost', port=6379, db=0)
+        length = len(items)
+        for i in range(length - 1):
+            right_idx = i + N - 1
+            prefix = tuple(items[i:right_idx])
 
-    items.insert(0, BEGIN) # add our begin and stop to our quote source
-    # TODO: add this end to the end of every sentence inside the quote
-    items.append(END)
+            # we cycle back to the beginning of the quote if the last index overflows
+            # this helps prevents prematurely ending the markov stream
+            suffix = items[right_idx % length] 
+            self.store_ngram(prefix, suffix, True)
 
-    for i in range(len(items) - 1):
-        right_idx = i + N - 1
-        prefix = tuple(items[i:right_idx])
-        suffix = items[right_idx] 
-        store_ngram(storage, prefix, suffix, True)
-        if items[right_idx] == END:
-            break
+        if debug return self.storage
 
-    if debug:
-        return storage
+    def get_seed(self):
+        if debug:
+            starts = self.storage.keys()
+            return random.choice(starts)
 
-# TODO; integrate markov generator with bot
-
-def text_generate():
-    pass
-
-def random_selection():
-    pass
+    def get_storage(self):
+        # returns redis client or dict
+        pass
--- markov/tests.py
+++ markov/tests.py
@@ -1,23 +1,25 @@
 import unittest
-import markov
+from markov import Markov
 
 
 class TestNGrams(unittest.TestCase):
     """
     Test n-grams function with various quotes and strings
     """
+    @classmethod
+    def setUpClass(self):
+        self.markov = Markov({}, True)
+        self.seed_string = 'to be or not to be just be who you want to be or not ok you want okay'
 
     def test_store_ngram(self):
         test_start = ('to', 'be')
-        self.assertEqual(markov.store_ngram({}, test_start, 'or', True), {
+        self.assertEqual(self.markov.store_ngram({}, test_start, 'or', True), {
             test_start: {'or': 1, 'total_suffixes': 1}
         })
 
     def test_n_gram_quote(self):
-        self.maxDiff = None
-        print(markov.n_gram('to be or not to be just be who you want to be or not ok you want okay', True))
-        self.assertEqual(markov.n_gram(
-            'to be or not to be just be who you want to be or not ok you want okay', True),  {
+        self.assertEqual(self.markov.generate_n_gram(
+            self.seed_string),  {
                 ('to', 'be'): {'or': 2, 'just': 1, 'total_suffixes': 3},
                 ('be',  'or'): {'not': 2, 'total_suffixes': 2 },
                 ('or',  'not'): {'to': 1, 'ok': 1, 'total_suffixes': 2},
@@ -30,8 +32,14 @@ class TestNGrams(unittest.TestCase):
                 ('want', 'to'): {'be': 1, 'total_suffixes': 1},
                 ('not', 'ok'): {'you': 1, 'total_suffixes': 1},
                 ('ok', 'you'): {'want': 1, 'total_suffixes': 1},
-                ('want', 'okay'): {'_END_': 1, 'total_suffixes': 1},
+                ('want', 'okay'): {'to': 1, 'total_suffixes': 1},
+                ('okay', 'to'): {'be': 1, 'total_suffixes': 1 }
         })
+
+    def test_n_gram_get_seed(self):
+        # makes sure our get_seed function returns an actual seed
+        self.markov.generate_n_gram(self.seed_string, True)
+        self.assertIn(self.markov.get_seed(), self.seed_string)
 
 if __name__ == '__main__':
     unittest.main()
